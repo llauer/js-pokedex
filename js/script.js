a9eb776735cc6c4673604d1772a510a1e07cb4b3
Larry Lauer <larry.lauer.3d@gmail.com>
2019-02-09 00:13:16 -0700

    starting Pokemon repository

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+//pokemon repository JS app.
 
-//simple alert
-alert('Hello World');
+var repository = [
+  {name: "Bulbasaur", height: '0.7 m', type: ['grass', 'poison']},
+  {name: "Ivysaur", height: '1 m', type: ['grass', 'poison']},
+  {name: "Wartortle", height: '1 m', type: 'water'}
+]
