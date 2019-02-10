a7149ca94d03fef872e083b4d51475fe48761942
Larry Lauer <larry.lauer.3d@gmail.com>
2019-02-10 01:45:47 -0700

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,7 +1,35 @@
 //pokemon repository JS app.
 
+// let repository = [
+//   {"name":"Bulbasaur", "height":"0.7 m", "type":["grass", "poison"]},
+//   {"name":"Ivysaur", "height": "1 m", "type":["grass", "poison"]},
+//   {"name":"Wartortle", "height":"1 m", "type":"water"}
+// ];
+
 var repository = [
-  {name: "Bulbasaur", height: '0.7 m', type: ['grass', 'poison']},
-  {name: "Ivysaur", height: '1 m', type: ['grass', 'poison']},
-  {name: "Wartortle", height: '1 m', type: 'water'}
-]
+  {
+    name: 'Bulbasaur',
+    height: '0.7m',
+    type: ['grass', 'poison']
+  },
+  {
+    name: 'Ivysaur',
+    height: '1m',
+    type: ['grass', 'poison']
+  },
+  {
+    name: 'Wartortle',
+    height: '1m',
+    type: 'water'
+  }
+];
+repository.forEach(function(repo) {
+  
+    for(var prop in repo){
+        document.write('\n')
+   
+        document.write(prop + ': ' + repo[prop]);
+        document.write("<br><br>")
+  }
+ 
+});
